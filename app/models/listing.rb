4dd49781bd74f19b0393c7f21388a404c6287954
hazim0001 <hazim_alsayed@hotmail.com>
2021-02-27 01:44:26 +0200

    geocoder settings

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,4 @@
 class Listing < ApplicationRecord
-  geocoded_by :address
-  after_validation :geocode, if: :will_save_change_to_address?
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
 end
