060106a60cc2328eed48620f5fc77d9391917f3f
hazim0001 <hazim_alsayed@hotmail.com>
2021-02-27 04:43:23 +0200

    front end

--- app/views/listings/_listing.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! listing, :id, :location, :bds, :ba, :area, :developer, :price, :created_at, :updated_at
-json.url listing_url(listing, format: :json)
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Listing.destroy_all
+devs = %w[Emaar PalmHills SODIC]
+counter = 0
+
+50.times do
+  Listing.create(
+    location: Faker::Address.street_address,
+    bds: rand(1..5),
+    ba: rand(1..3),
+    area: rand(90..490),
+    developer: devs.sample,
+    price: rand(900_000..100_000_00)
+  )
+  counter += 1
+  puts "Listing no #{counter} has been created"
+end
